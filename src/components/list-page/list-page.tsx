323ca0f36e246a57a288eb41cb649d6cfb2ceb16
pankiale <93738391+pankiale@users.noreply.github.com>
2023-03-05 17:10:58 +0100

    wip

--- src/components/list-page/list-page.tsx
+++ src/components/list-page/list-page.tsx
@@ -1,10 +1,29 @@
-import React from "react";
-import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import React, {useState} from "react";
+import {SolutionLayout} from "../ui/solution-layout/solution-layout";
+import {LinkedList} from "../../utils/linked-list";
+import styles from "../list-page/list.module.css";
+import {Circle} from "../ui/circle/circle";
+import {ElementStates} from "../../types/element-states";
+import {ArrowIcon} from "../ui/icons/arrow-icon";
 
 export const ListPage: React.FC = () => {
-  return (
-    <SolutionLayout title="Связный список">
 
-    </SolutionLayout>
-  );
+    const [list, setList] = useState(LinkedList.fromArray([0, 32, 8, 1]));
+
+
+    console.log(list)
+    console.log(list.print())
+    return (
+        <SolutionLayout title="Связный список">
+            <div className={styles.textbox}>
+                {list.print().map((item, index) => <React.Fragment key={index}><Circle
+                        head={item.isHead ? 'head' : null} tail={item.isTail ? 'tail' : null} index={index}
+                        letter={item.element.toString()}
+                        state={ElementStates.Default}/> <ArrowIcon/>
+                    </React.Fragment>
+                )}
+            </div>
+        </SolutionLayout>
+    );
 };
+
---
+++ src/utils/linked-list.ts
@@ -0,0 +1,178 @@
+class ListNode<T> {
+    data: T;
+    next: ListNode<T> | null;
+
+    constructor(data: T) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+export class LinkedList<T> {
+    head: ListNode<T> | null;
+    size: number;
+
+    constructor() {
+        this.head = null;
+        this.size = 0;
+    }
+
+    static fromArray<T>(arr: T[]): LinkedList<T> {
+        const list = new LinkedList<T>();
+        for (const val of arr) {
+            list.add(val);
+        }
+        return list;
+    }
+
+    // Add element to the end of the list
+    add(data: T): void {
+        const newNode = new ListNode(data);
+        if (this.head === null) {
+            this.head = newNode;
+        } else {
+            let current = this.head;
+            while (current.next !== null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+        this.size++;
+    }
+
+    addToTail(data: T): void {
+        const newNode = new ListNode<T>(data);
+        if (!this.head) {
+            this.head = newNode;
+            return;
+        }
+        let current = this.head;
+        while (current.next) {
+            current = current.next;
+        }
+        current.next = newNode;
+        this.size++;
+    }
+
+    // Insert element at a given position
+    insertAt(data: T, position: number): boolean {
+        if (position < 0 || position > this.size) {
+            return false;
+        }
+        const newNode = new ListNode(data);
+        let current = this.head;
+        let previous: ListNode<T> | null = null;
+        let index = 0;
+        if (position === 0) {
+            newNode.next = current;
+            this.head = newNode;
+        } else {
+            while (index < position) {
+                previous = current;
+                current = current!.next;
+                index++;
+            }
+            newNode.next = current;
+            previous!.next = newNode;
+        }
+        this.size++;
+        return true;
+    }
+
+    // Remove element from a given position
+    removeFrom(position: number): T | null {
+        if (position < 0 || position >= this.size) {
+            return null;
+        }
+        let current = this.head;
+        let previous: ListNode<T> | null = null;
+        let index = 0;
+        if (position === 0) {
+            this.head = current!.next;
+        } else {
+            while (index < position) {
+                previous = current;
+                current = current!.next!;
+                index++;
+            }
+            previous!.next = current!.next;
+        }
+        this.size--;
+        return current!.data;
+    }
+
+    // Remove element with given value
+    removeElement(data: T): T | null {
+        let current = this.head;
+        let previous: ListNode<T> | null = null;
+        while (current !== null) {
+            if (current.data === data) {
+                if (previous === null) {
+                    this.head = current.next;
+                } else {
+                    previous.next = current.next;
+                }
+                this.size--;
+                return current.data;
+            }
+            previous = current;
+            current = current.next;
+        }
+        return null;
+    }
+
+    // Get the element at a given position
+    get(position: number): T | null {
+        if (position < 0 || position >= this.size) {
+            return null;
+        }
+        let current = this.head;
+        let index = 0;
+        while (index < position) {
+            current = current!.next!;
+            index++;
+        }
+        return current!.data;
+    }
+
+    // Get the index of an element with given value
+    indexOf(data: T): number {
+        let current = this.head;
+        let index = 0;
+        while (current !== null) {
+            if (current.data === data) {
+                return index;
+            }
+            current = current.next;
+            index++;
+        }
+        return -1;
+    }
+
+    // Check if the list is empty
+    isEmpty(): boolean {
+        return this.size === 0;
+    }
+
+// Get the size of the list
+    public getSize(): number {
+        return this.size;
+    }
+
+    public print(): { element: T, index: number, isHead: boolean, isTail: boolean }[] {
+        let current = this.head;
+        const elements: { element: T, index: number, isHead: boolean, isTail: boolean }[] = [];
+        let index = 0;
+        while (current !== null) {
+            elements.push({
+                element: current.data,
+                index,
+                isHead: current === this.head,
+                isTail: current.next === null
+            });
+            current = current.next;
+            index++;
+        }
+        return elements;
+    }
+}
