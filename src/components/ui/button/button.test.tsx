507cc336b3f654b72e90e05fb54d5ff3741fe9d7
pankiale <93738391+pankiale@users.noreply.github.com>
2023-03-20 19:01:10 +0300

    queue

---
+++ src/components/ui/button/button.test.tsx
@@ -0,0 +1,39 @@
+import renderer from 'react-test-renderer';
+import {fireEvent, render, screen} from "@testing-library/react";
+import {Button} from "./button";
+
+const buttonTextStub = 'Текст кнопки'
+
+describe("button tests", () => {
+    it('button with text', () => {
+        const tree = renderer
+            .create(<Button text={buttonTextStub}/>)
+            .toJSON();
+        expect(tree).toMatchSnapshot();
+    })
+    it('button without text', () => {
+        const tree = renderer
+            .create(<Button/>)
+            .toJSON();
+        expect(tree).toMatchSnapshot();
+    })
+    it('button disabled', () => {
+        const tree = renderer
+            .create(<Button disabled/>)
+            .toJSON();
+        expect(tree).toMatchSnapshot();
+    })
+    it('button loading', () => {
+        const tree = renderer
+            .create(<Button isLoader/>)
+            .toJSON();
+        expect(tree).toMatchSnapshot();
+    })
+    it('button onclick', () => {
+        const mockOnClick = jest.fn();
+        render(<Button text={"testText"} onClick={mockOnClick}/>)
+        const button = screen.getByText('testText');
+        fireEvent.click(button);
+        expect(mockOnClick).toHaveBeenCalled();
+    })
+})
